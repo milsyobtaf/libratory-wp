5369a5cd422347b346d9800501af4622142654a4
Dustin Younse <milsyobtaf@gmail.com>
2013-02-12 17:06:11 -0600

    added bigfoot 404 page

--- 404.php
+++ 404.php
@@ -1,43 +1,47 @@
 <?php get_header(); ?>
-			
+
 			<div id="content">
 
 				<div id="inner-content" class="wrap clearfix">
-			
-					<div id="main" class="eightcol first clearfix" role="main">
+
+					<div id="main" class="error404 clearfix" role="main">
 
 						<article id="post-not-found" class="hentry clearfix">
-						
+
 							<header class="article-header">
-							
-								<h1><?php _e("Epic 404 - Article Not Found", "bonestheme"); ?></h1>
-						
+
+								<h1><?php _e("404 - Information Not Found", "bonestheme"); ?></h1>
+
 							</header> <!-- end article header -->
-					
+
 							<section class="entry-content">
-							
-								<p><?php _e("The article you were looking for was not found, but maybe try looking again!", "bonestheme"); ?></p>
-					
+
+								<p><?php _e("Like Bigfoot, the information you were looking for was not found.", "bonestheme"); ?></p>
+
+								<img src="<?php echo get_template_directory_uri(); ?>/library/images/bigfoot.jpg" />
+
+								<p><?php _e("But like Bigfoot, we believe your information is out there! Continue the search!","bonestheme"); ?></p>
+
 							</section> <!-- end article section -->
 
 							<section class="search">
-				
+
 							    <p><?php get_search_form(); ?></p>
-				
+
 							</section> <!-- end search section -->
-						
+
 							<footer class="article-footer">
-							
-							    <p><?php _e("This is the 404.php template.", "bonestheme"); ?></p>
-							
+
+							    <p><a href="http://www.cryptozoology.com/cryptids.php"><?php _e("Further information on Cryptids like Bigfoot and Nessie", "bonestheme"); ?></a></p>
+
 							</footer> <!-- end article footer -->
-					
+
 						</article> <!-- end article -->
-			
+
 					</div> <!-- end #main -->
 
 				</div> <!-- end #inner-content -->
-    
+
 			</div> <!-- end #content -->
 
 <?php get_footer(); ?>
